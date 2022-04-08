68c2faa86cde950ee9ccb7209218cfb082041002
Buddhike Kuruppu <67339257+Buddhika-Kuruppu@users.noreply.github.com>
2022-04-09 01:02:32 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+#configuration details of providers
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=0.13"
+    }
+  }
+}
+
+#providers configuration details for azure terraform
+
+provider "azurerm" {
+  features {}
+}
+
+# IaC to build a resource group
+
+resource "azurerm_resource_group" "res_g" {
+  name     = "basic_rg"
+  location = "southcentralus"
+  tags = {
+    envionment = "basic"
+    source     = "Terraform"
+    owner      = "buddhike"
+  }
+
+}
