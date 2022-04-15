a89ca730b80d39220b0f803b062b85c596634ebc
Buddhike Kuruppu <67339257+Buddhika-Kuruppu@users.noreply.github.com>
2022-04-16 03:37:29 +0530

    v1.1
    Commented v1.1

--- VM Cluster/vmcluster.tf
+++ VM Cluster/vmcluster.tf
@@ -14,99 +14,101 @@ terraform {
    features {}
  }
 
- resource "azurerm_resource_group" "test" {
-   name     = "acctestrg"
-   location = "West US 2"
+ resource "azurerm_resource_group" "redun_set" {
+   name     = "redundantrg"
+   location = "Southeast Asia"
  }
 
- resource "azurerm_virtual_network" "test" {
-   name                = "acctvn"
+ resource "azurerm_virtual_network" "redun_vnet" {
+   name                = "redundantvnet"
    address_space       = ["10.0.0.0/16"]
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
+   location            = azurerm_resource_group.redun_set.location
+   resource_group_name = azurerm_resource_group.redun_set.name
  }
 
- resource "azurerm_subnet" "test" {
-   name                 = "acctsub"
-   resource_group_name  = azurerm_resource_group.test.name
-   virtual_network_name = azurerm_virtual_network.test.name
-   address_prefixes     = ["10.0.2.0/24"]
+ resource "azurerm_subnet" "redun_snet" {
+   name                 = "redundantsnet"
+   resource_group_name  = azurerm_resource_group.redun_set.name
+   virtual_network_name = azurerm_virtual_network.redun_vnet.name
+   address_prefixes     = ["10.0.1.0/24"]
  }
 
- resource "azurerm_public_ip" "test" {
-   name                         = "publicIPForLB"
-   location                     = azurerm_resource_group.test.location
-   resource_group_name          = azurerm_resource_group.test.name
+ resource "azurerm_public_ip" "pip" {
+   name                         = "PIPforNLB"
+   location                     = azurerm_resource_group.redun_set.location
+   resource_group_name          = azurerm_resource_group.redun_set.name
    allocation_method            = "Static"
  }
 
- resource "azurerm_lb" "test" {
-   name                = "loadBalancer"
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
+ resource "azurerm_lb" "NLB" {
+   name                = "NLB"
+   location            = azurerm_resource_group.redun_set.location
+   resource_group_name = azurerm_resource_group.redun_set.name
 
    frontend_ip_configuration {
-     name                 = "publicIPAddress"
-     public_ip_address_id = azurerm_public_ip.test.id
+     name                 = "NLBfrontPIP"
+     public_ip_address_id = azurerm_public_ip.pip.id
    }
  }
 
- resource "azurerm_lb_backend_address_pool" "test" {
-   loadbalancer_id     = azurerm_lb.test.id
+ resource "azurerm_lb_backend_address_pool" "BackIP" {
+   loadbalancer_id     = azurerm_lb.NLB.id
    name                = "BackEndAddressPool"
  }
 
- resource "azurerm_network_interface" "test" {
+ resource "azurerm_network_interface" "nic" {
    count               = 2
-   name                = "acctni${count.index}"
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
+   name                = "vnic${count.index}"
+   location            = azurerm_resource_group.redun_set.location
+   resource_group_name = azurerm_resource_group.redun_set.name
 
    ip_configuration {
-     name                          = "testConfiguration"
-     subnet_id                     = azurerm_subnet.test.id
+     name                          = "BaseConfig"
+     subnet_id                     = azurerm_subnet.redun_snet.id
      private_ip_address_allocation = "dynamic"
    }
  }
-
- resource "azurerm_managed_disk" "test" {
+ resource "azurerm_managed_disk" "mdisk" {
    count                = 2
    name                 = "datadisk_existing_${count.index}"
-   location             = azurerm_resource_group.test.location
-   resource_group_name  = azurerm_resource_group.test.name
+   location             = azurerm_resource_group.redun_set.location
+   resource_group_name  = azurerm_resource_group.redun_set.name
    storage_account_type = "Standard_LRS"
    create_option        = "Empty"
-   disk_size_gb         = "1023"
+   disk_size_gb         = "20"
  }
 
  resource "azurerm_availability_set" "avset" {
    name                         = "avset"
-   location                     = azurerm_resource_group.test.location
-   resource_group_name          = azurerm_resource_group.test.name
+   location                     = azurerm_resource_group.redun_set.location
+   resource_group_name          = azurerm_resource_group.redun_set.name
    platform_fault_domain_count  = 2
-   platform_update_domain_count = 2
+   platform_update_domain_count = 5
    managed                      = true
  }
+#==================================================
+#change this resource as per required VM parameters
+#==================================================
 
- resource "azurerm_virtual_machine" "test" {
+ resource "azurerm_virtual_machine" "VM" {
    count                 = 2
-   name                  = "acctvm${count.index}"
-   location              = azurerm_resource_group.test.location
+   name                  = "VM${count.index}"
+   location              = azurerm_resource_group.redun_set.location
    availability_set_id   = azurerm_availability_set.avset.id
-   resource_group_name   = azurerm_resource_group.test.name
-   network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
-   vm_size               = "Standard_DS1_v2"
+   resource_group_name   = azurerm_resource_group.redun_set.name
+   network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
+   vm_size               = "Standard_DS1_v2" 
 
    # Uncomment this line to delete the OS disk automatically when deleting the VM
-   # delete_os_disk_on_termination = true
+   delete_os_disk_on_termination = true
 
    # Uncomment this line to delete the data disks automatically when deleting the VM
-   # delete_data_disks_on_termination = true
+   delete_data_disks_on_termination = true
 
    storage_image_reference {
      publisher = "Canonical"
      offer     = "UbuntuServer"
-     sku       = "16.04-LTS"
+     sku       = "22.04-LTS"
      version   = "latest"
    }
 
@@ -118,26 +120,29 @@ terraform {
    }
 
    # Optional data disks
-   storage_data_disk {
-     name              = "datadisk_new_${count.index}"
-     managed_disk_type = "Standard_LRS"
-     create_option     = "Empty"
-     lun               = 0
-     disk_size_gb      = "1023"
-   }
+   #storage_data_disk {
+    # name              = "datadisk_new_${count.index}"
+     #managed_disk_type = "Standard_LRS"
+     #create_option     = "Empty"
+     #lun               = 0
+     #disk_size_gb      = "1023"
+   #}
 
-   storage_data_disk {
-     name            = element(azurerm_managed_disk.test.*.name, count.index)
-     managed_disk_id = element(azurerm_managed_disk.test.*.id, count.index)
-     create_option   = "Attach"
-     lun             = 1
-     disk_size_gb    = element(azurerm_managed_disk.test.*.disk_size_gb, count.index)
-   }
+   #storage_data_disk {
+     #name            = element(azurerm_managed_disk.mdisk.*.name, count.index)
+     #managed_disk_id = element(azurerm_managed_disk.mdisk.*.id, count.index)
+     #create_option   = "Attach"
+     #lun             = 1
+     #disk_size_gb    = element(azurerm_managed_disk.mdisk.*.disk_size_gb, count.index)
+   #}
+#===================================================
+# Change here according to requirements of OS logins
+#===================================================
 
    os_profile {
-     computer_name  = "hostname"
-     admin_username = "testadmin"
-     admin_password = "Password1234!"
+     computer_name  = "hostname" 
+     admin_username = "admin"
+     admin_password = "admin123"
    }
 
    os_profile_linux_config {
@@ -145,6 +150,6 @@ terraform {
    }
 
    tags = {
-     environment = "staging"
+     environment = "basic_avset_vm_template"
    }
  }
