efad5d887bd6643b14bee0e380b846aeeb3e1fd6
Buddhike Kuruppu <67339257+Buddhika-Kuruppu@users.noreply.github.com>
2022-04-28 02:36:27 +0530

    validated v.14
    v.1.4

--- GR Deployments/prod-dr-az-2-az.tf
+++ GR Deployments/prod-dr-az-2-az.tf
@@ -1,19 +1,19 @@
 
 terraform {
 
-   required_version = ">=0.12"
+  required_version = ">=0.12"
 
-   required_providers {
-     azurerm = {
-       source = "hashicorp/azurerm"
-       version = "~>2.0"
-     }
-   }
- }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
 
- provider "azurerm" {
-   features {}
- }
+provider "azurerm" {
+  features {}
+}
 
 
 #----------------------
@@ -50,14 +50,14 @@ resource "azurerm_virtual_network" "dr" {
 resource "azurerm_subnet" "production" {
   name                 = "primary-subnet"
   resource_group_name  = azurerm_resource_group.production.name
-  virtual_network_name = azurerm_virtual_network.primary-vnet.name
+  virtual_network_name = azurerm_virtual_network.production.name
   address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_subnet" "dr" {
   name                 = "dr-subnet"
   resource_group_name  = azurerm_resource_group.dr.name
-  virtual_network_name = azurerm_virtual_network.dr-vnet.name
+  virtual_network_name = azurerm_virtual_network.dr.name
   address_prefixes     = ["10.2.1.0/24"]
 }
 
@@ -89,28 +89,28 @@ resource "azurerm_virtual_machine" "vm" {
   vm_size               = "Standard_D2s_v3"
   network_interface_ids = [azurerm_network_interface.vm.id]
 
-  storage_image_reference {              # Provide OS preferences
-    publisher = "Canonical"     
+  storage_image_reference { # Provide OS preferences
+    publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "20.04-LTS"
     version   = "latest"
   }
 
-  storage_os_disk {                     # Disk preferences
-    name              = "vm-os-disk" 
+  storage_os_disk { # Disk preferences
+    name              = "vm-os-disk"
     os_type           = "Linux"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
-  os_profile {                          # Login Credentials
-    admin_username = "admin"  
+  os_profile { # Login Credentials
+    admin_username = "admin"
     admin_password = "pa$$word"
     computer_name  = "vm"
   }
 
-  os_profile_linux_config {                     
+  os_profile_linux_config {
     disable_password_authentication = false
   }
 }
@@ -119,76 +119,76 @@ resource "azurerm_virtual_machine" "vm" {
 # Creatiion of ASR Vault
 #-----------------------
 
-resource "azurerm_recovery_services_vault" "ASR-vault" {
+resource "azurerm_recovery_services_vault" "ASR" {
   name                = "Site-Recovery-Vault"
   location            = azurerm_resource_group.dr.location
   resource_group_name = azurerm_resource_group.dr.name
   sku                 = "Standard"
 }
 
-resource "azurerm_site_recovery_fabric" "primary" {
+resource "azurerm_site_recovery_fabric" "production" {
   name                = "primary-fabric"
-  resource_group_name = azurerm_resource_group.secondary.name
-  recovery_vault_name = azurerm_recovery_services_vault.vault.name
-  location            = azurerm_resource_group.primary.location
+  resource_group_name = azurerm_resource_group.dr.name
+  recovery_vault_name = azurerm_recovery_services_vault.ASR.name
+  location            = azurerm_resource_group.production.location
 }
 
-resource "azurerm_site_recovery_fabric" "secondary" {
+resource "azurerm_site_recovery_fabric" "dr" {
   name                = "secondary-fabric"
-  resource_group_name = azurerm_resource_group.secondary.name
-  recovery_vault_name = azurerm_recovery_services_vault.vault.name
-  location            = azurerm_resource_group.secondary.location
+  resource_group_name = azurerm_resource_group.dr.name
+  recovery_vault_name = azurerm_recovery_services_vault.ASR.name
+  location            = azurerm_resource_group.dr.location
 }
 
-resource "azurerm_site_recovery_protection_container" "primary" {
+resource "azurerm_site_recovery_protection_container" "production" {
   name                 = "primary-protection-container"
-  resource_group_name  = azurerm_resource_group.secondary.name
-  recovery_vault_name  = azurerm_recovery_services_vault.vault.name
-  recovery_fabric_name = azurerm_site_recovery_fabric.primary.name
+  resource_group_name  = azurerm_resource_group.dr.name
+  recovery_vault_name  = azurerm_recovery_services_vault.ASR.name
+  recovery_fabric_name = azurerm_site_recovery_fabric.production.name
 }
 
-resource "azurerm_site_recovery_protection_container" "secondary" {
+resource "azurerm_site_recovery_protection_container" "dr" {
   name                 = "secondary-protection-container"
-  resource_group_name  = azurerm_resource_group.secondary.name
-  recovery_vault_name  = azurerm_recovery_services_vault.vault.name
-  recovery_fabric_name = azurerm_site_recovery_fabric.secondary.name
+  resource_group_name  = azurerm_resource_group.dr.name
+  recovery_vault_name  = azurerm_recovery_services_vault.ASR.name
+  recovery_fabric_name = azurerm_site_recovery_fabric.dr.name
 }
 
 resource "azurerm_site_recovery_replication_policy" "policy" {
   name                                                 = "policy"
-  resource_group_name                                  = azurerm_resource_group.secondary.name
-  recovery_vault_name                                  = azurerm_recovery_services_vault.vault.name
+  resource_group_name                                  = azurerm_resource_group.dr.name
+  recovery_vault_name                                  = azurerm_recovery_services_vault.ASR.name
   recovery_point_retention_in_minutes                  = 24 * 60
   application_consistent_snapshot_frequency_in_minutes = 4 * 60
 }
 
 resource "azurerm_site_recovery_protection_container_mapping" "container-mapping" {
   name                                      = "container-mapping"
-  resource_group_name                       = azurerm_resource_group.secondary.name
-  recovery_vault_name                       = azurerm_recovery_services_vault.vault.name
-  recovery_fabric_name                      = azurerm_site_recovery_fabric.primary.name
-  recovery_source_protection_container_name = azurerm_site_recovery_protection_container.primary.name
-  recovery_target_protection_container_id   = azurerm_site_recovery_protection_container.secondary.id
+  resource_group_name                       = azurerm_resource_group.dr.name
+  recovery_vault_name                       = azurerm_recovery_services_vault.ASR.name
+  recovery_fabric_name                      = azurerm_site_recovery_fabric.production.name
+  recovery_source_protection_container_name = azurerm_site_recovery_protection_container.production.name
+  recovery_target_protection_container_id   = azurerm_site_recovery_protection_container.dr.id
   recovery_replication_policy_id            = azurerm_site_recovery_replication_policy.policy.id
 }
 
 resource "azurerm_site_recovery_network_mapping" "network-mapping" {
   name                        = "network-mapping"
-  resource_group_name         = azurerm_resource_group.secondary.name
-  recovery_vault_name         = azurerm_recovery_services_vault.vault.name
-  source_recovery_fabric_name = azurerm_site_recovery_fabric.primary.name
-  target_recovery_fabric_name = azurerm_site_recovery_fabric.secondary.name
-  source_network_id           = azurerm_virtual_network.primary.id
-  target_network_id           = azurerm_virtual_network.secondary.id
+  resource_group_name         = azurerm_resource_group.dr.name
+  recovery_vault_name         = azurerm_recovery_services_vault.ASR.name
+  source_recovery_fabric_name = azurerm_site_recovery_fabric.production.name
+  target_recovery_fabric_name = azurerm_site_recovery_fabric.dr.name
+  source_network_id           = azurerm_virtual_network.production.id
+  target_network_id           = azurerm_virtual_network.dr.id
 }
 
 #------------------------------------------
 # Primary Site Storage Accounts  - Optional
 #------------------------------------------
-resource "azurerm_storage_account" "primary" {
+resource "azurerm_storage_account" "production" {
   name                     = "primaryrecoverycache"
-  location                 = azurerm_resource_group.primary.location
-  resource_group_name      = azurerm_resource_group.primary.name
+  location                 = azurerm_resource_group.production.location
+  resource_group_name      = azurerm_resource_group.production.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -197,14 +197,14 @@ resource "azurerm_storage_account" "primary" {
 
 resource "azurerm_network_interface" "vm" {
   name                = "vm-nic"
-  location            = azurerm_resource_group.primary.location
-  resource_group_name = azurerm_resource_group.primary.name
+  location            = azurerm_resource_group.production.location
+  resource_group_name = azurerm_resource_group.production.name
 
   ip_configuration {
     name                          = "vm"
-    subnet_id                     = azurerm_subnet.primary.id
+    subnet_id                     = azurerm_subnet.production.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.primary.id
+    public_ip_address_id          = azurerm_public_ip.production.id
   }
 }
 
@@ -215,22 +215,22 @@ resource "azurerm_network_interface" "vm" {
 resource "azurerm_site_recovery_replicated_vm" "vm-replication" {
   name                                      = "vm-replication"
   resource_group_name                       = azurerm_resource_group.dr.name
-  recovery_vault_name                       = azurerm_recovery_services_vault.ASR-vault.name
-  source_recovery_fabric_name               = azurerm_site_recovery_fabric.primary.name
+  recovery_vault_name                       = azurerm_recovery_services_vault.ASR.name
+  source_recovery_fabric_name               = azurerm_site_recovery_fabric.production.name
   source_vm_id                              = azurerm_virtual_machine.vm.id
   recovery_replication_policy_id            = azurerm_site_recovery_replication_policy.policy.id
-  source_recovery_protection_container_name = azurerm_site_recovery_protection_container.primary.name
+  source_recovery_protection_container_name = azurerm_site_recovery_protection_container.production.name
 
-  target_resource_group_id                = azurerm_resource_group.secondary.id
-  target_recovery_fabric_id               = azurerm_site_recovery_fabric.secondary.id
-  target_recovery_protection_container_id = azurerm_site_recovery_protection_container.secondary.id
+  target_resource_group_id                = azurerm_resource_group.dr.id
+  target_recovery_fabric_id               = azurerm_site_recovery_fabric.dr.id
+  target_recovery_protection_container_id = azurerm_site_recovery_protection_container.dr.id
 
   managed_disk {
     disk_id                    = azurerm_virtual_machine.vm.storage_os_disk[0].managed_disk_id
-    staging_storage_account_id = azurerm_storage_account.primary.id
-    target_resource_group_id   = azurerm_resource_group.secondary.id
-    target_disk_type           = "Premium_LRS"
-    target_replica_disk_type   = "Premium_LRS"
+    staging_storage_account_id = azurerm_storage_account.production.id
+    target_resource_group_id   = azurerm_resource_group.dr.id
+    target_disk_type           = "Standard_LRS"
+    target_replica_disk_type   = "Standard_LRS"
   }
 
   network_interface {
