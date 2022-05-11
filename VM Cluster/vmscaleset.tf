2a0de1899af201e268c8c3f226de03fe28bf729c
Buddhike Kuruppu <67339257+Buddhika-Kuruppu@users.noreply.github.com>
2022-05-12 00:45:48 +0530

    rev1.1
    Add Comments

--- VM Cluster/vmscaleset.tf
+++ VM Cluster/vmscaleset.tf
@@ -1,68 +1,72 @@
+#-----------------------------------------------------
 #create VM scale set with LB for VM with Managed Disks
 #-----------------------------------------------------
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "prodrg" {
+  name     = "all-resources"
+  location = "South India"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "acctvn"
+resource "azurerm_virtual_network" "prodvnet" {
+  name                = "vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.prodrg.location
+  resource_group_name = azurerm_resource_group.prodrg.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "prodsnet" {
   name                 = "acctsub"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+  resource_group_name  = azurerm_resource_group.prodrg.name
+  virtual_network_name = azurerm_virtual_network.prodvnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "test"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_public_ip" "prodpip" {
+  name                = "pip"
+  location            = azurerm_resource_group.prodrg.location
+  resource_group_name = azurerm_resource_group.prodrg.name
   allocation_method   = "Static"
-  domain_name_label   = azurerm_resource_group.example.name
+  domain_name_label   = azurerm_resource_group.prodrg.name
 
   tags = {
-    environment = "staging"
+    environment = "development"
+    Purpose = "Base Template"
   }
 }
 
-resource "azurerm_lb" "example" {
-  name                = "test"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_lb" "prodlb" {
+  name                = "ELB"
+  location            = azurerm_resource_group.prodrg.location
+  resource_group_name = azurerm_resource_group.prodrg.name
 
   frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    name                 = "PublicIPEnd"
+    public_ip_address_id = azurerm_public_ip.prodpip.id
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "bpepool" {
-  resource_group_name = azurerm_resource_group.example.name
-  loadbalancer_id     = azurerm_lb.example.id
-  name                = "BackEndAddressPool"
+resource "azurerm_lb_backend_address_pool" "backendpool" {
+  resource_group_name = azurerm_resource_group.prodrg.name
+  loadbalancer_id     = azurerm_lb.prodlb.id
+  name                = "BackendIPPool"
 }
+
+#Completed up to here above
 
 resource "azurerm_lb_nat_pool" "lbnatpool" {
-  resource_group_name            = azurerm_resource_group.example.name
-  name                           = "ssh"
-  loadbalancer_id                = azurerm_lb.example.id
-  protocol                       = "Tcp"
-  frontend_port_start            = 50000
-  frontend_port_end              = 50119
+  resource_group_name            = azurerm_resource_group.prodrg.name
+  name                           = "for_secure_access"
+  loadbalancer_id                = azurerm_lb.prodlb.id
+  protocol                       = "TCP"
+  frontend_port_start            = 1
+  frontend_port_end              = 60000
   backend_port                   = 22
-  frontend_ip_configuration_name = "PublicIPAddress"
+  frontend_ip_configuration_name = "PIPAddress"
 }
 
-resource "azurerm_lb_probe" "example" {
-  resource_group_name = azurerm_resource_group.example.name
-  loadbalancer_id     = azurerm_lb.example.id
+resource "azurerm_lb_probe" "lbprobe" {
+  resource_group_name = azurerm_resource_group.prodrg.name
+  loadbalancer_id     = azurerm_lb.prodlb.id
   name                = "http-probe"
   protocol            = "Http"
   request_path        = "/health"
@@ -72,10 +76,10 @@ resource "azurerm_lb_probe" "example" {
 # Defining Azure Scale Set parameters
 #------------------------------------
 
-resource "azurerm_virtual_machine_scale_set" "example" {
-  name                = "mytestscaleset-1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_machine_scale_set" "vmscset" {
+  name                = "webserver-scset"
+  location            = azurerm_resource_group.prodrg.location
+  resource_group_name = azurerm_resource_group.prodrg.name
 
   # automatic rolling upgrade
   automatic_os_upgrade = true
@@ -89,10 +93,10 @@ resource "azurerm_virtual_machine_scale_set" "example" {
   }
 
   # required when using rolling upgrade policy
-  health_probe_id = azurerm_lb_probe.example.id
+  health_probe_id = azurerm_lb_probe.lbprobe.id
 
   sku {
-    name     = "Standard_F2"
+    name     = "Standard_D2s_v3"
     tier     = "Standard"
     capacity = 2
   }
@@ -100,12 +104,12 @@ resource "azurerm_virtual_machine_scale_set" "example" {
   storage_profile_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
   storage_profile_os_disk {
-    name              = ""
+    name              = "os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -119,15 +123,16 @@ resource "azurerm_virtual_machine_scale_set" "example" {
   }
 
   os_profile {
-    computer_name_prefix = "testvm"
-    admin_username       = "myadmin"
+    computer_name_prefix = "WEB"
+    admin_username       = "webadmin"
+    admin_password       = "testpassword"
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
 
     ssh_keys {
-      path     = "/home/myadmin/.ssh/authorized_keys"
+      path     = "/home/webadmin/.ssh/authorized_keys"
       key_data = file("~/.ssh/demo_key.pub")
     }
   }
@@ -137,10 +142,10 @@ resource "azurerm_virtual_machine_scale_set" "example" {
     primary = true
 
     ip_configuration {
-      name                                   = "TestIPConfiguration"
+      name                                   = "ScSetIPConfiguration"
       primary                                = true
-      subnet_id                              = azurerm_subnet.example.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      subnet_id                              = azurerm_subnet.prodsnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.backendpool.id]
       load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
     }
   }
@@ -154,26 +159,29 @@ resource "azurerm_virtual_machine_scale_set" "example" {
 # Auto Scaling Configuration
 #---------------------------
 
-resource "azurerm_monitor_autoscale_setting" "example" {
-  name                = "myAutoscaleSetting"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  target_resource_id  = azurerm_virtual_machine_scale_set.example.id
+resource "azurerm_monitor_autoscale_setting" "asconfig" {
+  name                = "AutoscaleConfig"
+  resource_group_name = azurerm_resource_group.prodrg.name
+  location            = azurerm_resource_group.prodrg.location
+  target_resource_id  = azurerm_virtual_machine_scale_set.vmscset.id
 
 # parameters for Auto-Scaling
+
   profile {
     name = "defaultProfile"
 
     capacity {
       default = 1
       minimum = 1
-      maximum = 10
+      maximum = 5
     }
+
+# Load Increasing Rule 
 
     rule {
       metric_trigger {
         metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_virtual_machine_scale_set.example.id
+        metric_resource_id = azurerm_virtual_machine_scale_set.vmscset.id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
@@ -196,10 +204,12 @@ resource "azurerm_monitor_autoscale_setting" "example" {
       }
     }
 
+# Load Decreasing Rule
+
     rule {
       metric_trigger {
         metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_virtual_machine_scale_set.example.id
+        metric_resource_id = azurerm_virtual_machine_scale_set.vmscset.id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
